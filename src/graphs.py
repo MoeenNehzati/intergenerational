8ffce248e09ef11d3465e54430c23eabb0c8b12f
Moeen Nehzati <nehzati@usc.edu>
2020-05-22 16:16:44 -0700

    It works now

--- src/graphs.py
+++ src/graphs.py
@@ -1,20 +1,32 @@
 from utils import *
+import seaborn as sns
+from matplotlib import pyplot as plt
 np.random.seed(11523)
 number_of_the_genes = 1000
-length = 660
+length = 30
 vg0 = np.eye(number_of_the_genes)/number_of_the_genes
 c = 0.5 - 0.5*np.eye(number_of_the_genes)
 a = np.random.normal(size = (1, number_of_the_genes))
 p = 0.5
 mu = 0.5
 ve = 1
-vg, vy, m = simulate_generations(length, vg0, c, a, p, mu, ve)
-vy
-np.array(vy[1:]).reshape((1,-1)) - np.array(vy[:-1]).reshape((1,-1))
-import seaborn as sns
-title = f'With mu={mu}, p={p}, Ve={ve}, number_of_the_genes={number_of_the_genes},\n Vg0=I{number_of_the_genes}^-1, A~N'
-fig = sns.lineplot("generation", "Vy", data=pd.DataFrame({"Vy":vy, "generation":range(length)}))
-fig.set_title(title)
-fig.get_figure().savefig("../out/figs/"+title.replace(" ", "").replace("\n", "")+".png")
-from matplotlib import pyplot as plt
-plt.show()
+
+def simulate_and_plot(length, vg0, c, a, p, mu, ve):
+    plt.clf()
+    vg, vy, m = simulate_generations(length, vg0, c, a, p, mu, ve)
+    title = f'Vy with mu={mu}, p={p}, Ve={ve}, number_of_the_genes={number_of_the_genes},\n Vg0=I{number_of_the_genes}^-1, A~N'
+    fig = sns.lineplot("generation", "Vy", data=pd.DataFrame({"Vy":vy, "generation":range(length)}))
+    fig.set_title(title)
+    fig.get_figure().savefig("../out/figs/"+title.replace(" ", "").replace("\n", "")+".png")
+    plt.clf()
+    title = f'h2 with mu={mu}, p={p}, Ve={ve}, number_of_the_genes={number_of_the_genes},\n Vg0=I{number_of_the_genes}^-1, A~N'
+    fig = sns.lineplot("generation", "h2", data=pd.DataFrame({"h2":[(a@vg[i]@a.T).item()/vy[i] for i in range(length)], "generation":range(length)}))
+    fig.set_title(title)
+    fig.get_figure().savefig("../out/figs/"+title.replace(" ", "").replace("\n", "")+".png")
+    plt.clf()
+
+simulate_and_plot(length, vg0, c, a, p, 0.25, ve)
+simulate_and_plot(length, vg0, c, a, p, 0.5, ve)
+simulate_and_plot(length, vg0, c, a, p, 1, ve)
+simulate_and_plot(length, vg0, c, a, p, 2, ve)
+
--- src/utils.py
+++ src/utils.py
@@ -2,14 +2,13 @@ import numpy as np
 import pandas as pd
 def compute_generation(index, vg, vy, m, c, a, p, mu, ve):
     i = index
-    vg[i] = (1-c)*vg[i-1] + m[i-1]/2 + c*m[i-2]
+    vg[i] = (1-c)*vg[i-1] + m[i-1]/2 + (2*c-1)*m[i-2]/2
     vy[i] = ve + a@(vg[i] + 2*mu*(1+mu)*vg[i-1] + 2*mu*(1+mu)*m[i-1])@a.T
     vy[i] = vy[i].item()
-    m[i] = (p/vy[i])*(vg[i] + mu*vg[i-1] + mu*m[i-1])@a.T@a@(vg[i] + mu*vg[i-1] + mu*m[i-1])/2
+    m[i] = (p/vy[i])*(vg[i] + mu*vg[i-1] + mu*m[i-1])@a.T@a@(vg[i] + mu*vg[i-1] + mu*m[i-1])
 
 
 def simulate_generations(length, vg0, c, a, p, mu, ve):  
-    print("in simulate_generations", vg0)  
     vg = [0 for i in range(length+2)]
     vy = [0 for i in range(length+2)]
     m = [np.zeros(vg0.shape) for i in range(length+2)]
